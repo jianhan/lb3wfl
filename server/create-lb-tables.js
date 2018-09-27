5b751745fc71bdf7de5c1c59d563b666fd144847
Jian Han <jianhan@mail.com>
2018-09-27 22:49:57 +1000

    [feat] Setup access token

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// const server = require('./server');
-// const ds = server.dataSources.mongodb;
-// const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
-// ds.automigrate(lbTables, (er) => {
-//   if (er) throw er;
-//   console.log('Tables [' + lbTables + '] created in ', ds.adapter.name);
-//   ds.disconnect();
-// });
+const server = require('./server');
+const ds = server.dataSources.mongodb;
+const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+ds.autoupdate(lbTables, (er) => {
+  if (er) throw er;
+  console.log('Tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
