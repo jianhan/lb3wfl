ede9afe0833ea318d115db5b96659d2d4647c731
Jian Han <jianhan@mail.com>
2018-09-27 21:43:59 +1000

    [feat] Added local datasources

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,8 +1,10 @@
-const server = require('./server');
-const ds = server.dataSources.mongodb;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
-ds.automigrate(lbTables, (er) => {
-  if (er) throw er;
-  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
-  ds.disconnect();
-});
+'use strict';
+
+// const server = require('./server');
+// const ds = server.dataSources.mongodb;
+// const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+// ds.automigrate(lbTables, (er) => {
+//   if (er) throw er;
+//   console.log('Tables [' + lbTables + '] created in ', ds.adapter.name);
+//   ds.disconnect();
+// });
